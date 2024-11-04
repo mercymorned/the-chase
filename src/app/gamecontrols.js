3102de7a308796bde85485c93e0391db12eb8c39
Ronan Griffin <ronanogriffin@gmail.com>
2024-11-05 12:29:02 +1300

    feat: create gamecontrols script file and import on main page, set variables for square color as the game progresses

---
+++ src/app/gamecontrols.js
@@ -0,0 +1,14 @@
+let money = "";
+
+const turquoise = "#00a9bb";
+const darkblue = "#00167c";
+const lightblue = "#01587a";
+const red = "#b4211a";
+const darkred = "6B0f06";
+
+let playerPosition = 0;
+let chaserPosition = -1;
+let gameStarted = false;
+
+
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./gamecontrols.js"
 
 export default function Home() {
   return (
