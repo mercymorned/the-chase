2c3c24ba8be3e4c94a525c23e72ad23b68d40b9c
Ronan Griffin <ronanogriffin@gmail.com>
2024-11-05 17:26:26 +1300

    feat: create Board component and assign number value to each Bar component rendered inside the Board

--- src/app/page.js
+++ src/app/page.js
@@ -5,32 +5,36 @@ import StartButton from './start-button.js'
 import AdvancePlayerButton from './advance-player-button.js';
 import AdvanceChaserButton from './advance-chaser-button.js'
 import { useState } from "react";
-import { useId } from "react";
 
 function Bar({value}) {
-  const id = useId();
-  return <div className={styles.square} id={id}></div>
+  return <div className={styles.square}></div>
 }
 
 function InputBar() {
-  const id = useId();
-  return <div className={styles.square} id={id}><input className={styles.squareinput}></input></div>
+  return <div className={styles.square}><input className={styles.squareinput}></input></div>
+}
+
+function Board() {
+  return (
+    <div className={styles.squares}>
+    <Bar value={1} />
+    <Bar value={2} />
+    <Bar value={3} />
+    <Bar value={4} />
+    <Bar value={5} />
+    <Bar value={6} />
+    <Bar value={7} />
+  </div>
+  );
 }
 
 export default function HomePage() {
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
 
-        <div className={styles.squares}>
-          <Bar />
-          <Bar />
-          <Bar />
-          <Bar />
-          <Bar />
-          <Bar />
-          <Bar />
-        </div>
+          <Board />
 
       </main>
       <footer className={styles.footer}>
