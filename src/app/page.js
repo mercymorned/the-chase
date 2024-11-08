1b33d0fba679cac39408a304814beb9c75d3ca16
Ronan Griffin <ronanogriffin@gmail.com>
2024-11-08 14:49:34 +1300

    feat: render Bar components as an array with ID assigned

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import styles from "./page.module.css";
-import Bar from "./components/bar.js";
+import Bar from "./components/bar-array.js";
 import InputBar from "./components/input-bar.js";
 import StartButton from './components/start-button.js'
 import AdvancePlayerButton from './components/advance-player-button.js';
@@ -9,26 +9,30 @@ import AdvanceChaserButton from './components/advance-chaser-button.js'
 import { useState } from "react";
 
 function Board() {
-  return (
+  const bars = [
+    { id: 1},
+    { id: 2},
+    { id: 3},
+    { id: 4},
+    { id: 5},
+    { id: 6},
+    { id: 7},
+  ];
+  return bars.map(bar =>
     <div className={styles.squares}>
-    <Bar value={1} />
-    <Bar value={2} />
-    <Bar value={3} />
-    <Bar value={4} />
-    <Bar value={5} />
-    <Bar value={6} />
-    <Bar value={7} />
-  </div>
-  );
+    <Bar key={(bar.id)} />
+    </div>
+  );                   
 }
 
 export default function HomePage() {
+  const boardKey = [1];
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
 
-          <Board />
+          <Board key={(boardKey)}/>
 
       </main>
       <footer className={styles.footer}>
