f09caedb5dcb35287cb1b4725764056223b0a00f
Ronan Griffin <ronanogriffin@gmail.com>
2024-12-05 11:41:40 +1300

    saving some attempts to define style, none of these work at changing style from player to chaser

--- src/app/components/bar-array.js
+++ src/app/components/bar-array.js
@@ -3,7 +3,16 @@
 import styles from ".././page.module.css";
 
 export default function Bar({barID, isPlayerOccupied, isChaserOccupied}) {
-  const style = isPlayerOccupied ? styles.wonQuestion : styles.currentQuestion && isChaserOccupied ? styles.chaser : styles.currentQuestion;
+  // const style = isPlayerOccupied ? styles.wonQuestion : styles.currentQuestion && isChaserOccupied ? styles.chaser : styles.currentQuestion;
+
+  // const style = (
+  //   isPlayerOccupied ? styles.wonQuestion :
+  //   isChaserOccupied ? styles.chaser :
+  //   (isPlayerOccupied && isChaserOccupied) ? styles.chaser :
+  //   styles.currentQuestion
+  // );
+
+  const style = (isPlayerOccupied ? styles.wonQuestion : styles.currentQuestion && isChaserOccupied ? styles.chaser : styles.currentQuestion && (isPlayerOccupied || isChaserOccupied) ? styles.chaser : styles.currentQuestion && styles.currentQuestion);
 
   return (
     <div className={style}>{barID}</div>
