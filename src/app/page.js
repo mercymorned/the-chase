7b8600eacfb065b950b0f19e750db80ac7c367b8
Ronan Griffin <ronanogriffin@gmail.com>
2024-12-10 14:45:40 +1300

    feat: make room for footer buttons in mobile layout
    also remove an unused method causing an error

--- src/app/page.js
+++ src/app/page.js
@@ -66,7 +66,6 @@ export function Board() {
     setPlayerPosition(playerPosition);
     setAsStartingBidBar((startingBidPosition), true);
     setStartingBidPosition(startingBidPosition);
-    clearHighBid();
   };
 
   const selectLowBid = () => {
